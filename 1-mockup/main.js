6002e89cd37ab3b36b6f699b91a77955eb5cada5
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2017-06-18 14:16:21 +0200

    1-mockup task end

--- 1-mockup/main.js
+++ 1-mockup/main.js
@@ -1,3 +1,26 @@
 /**
  * Created by Grzegorz on 18.06.2017.
  */
+class MockupElement extends HTMLElement {
+    constructor () {
+        super();
+       // console.log("Add custom mockupelement");
+        this.shadow = this.attachShadow({mode: 'open'});
+       // console.log(this.shadow);
+    }
+    connectedCallback() {
+        //console.log('MockupElement#connectedCallback');
+        this.template = document.querySelector('template');
+        //console.log(this.template);
+        //let $template =  document.querySelector('#mockup-template').content.cloneNode(true);
+        let template = document.currentScript.ownerDocument.querySelector('#mockup-template').content.cloneNode(true);
+
+         this.shadow.appendChild(template);
+         let photo = this.attributes.photo.value;
+         this.shadow.querySelector('img').setAttribute('src', photo);
+
+         let label = this.attributes.label.value;
+         this.shadow.querySelector('h1').textContent = label;
+    }
+}
+window.customElements.define('mockup-element', MockupElement);
